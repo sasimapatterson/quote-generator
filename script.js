5041d1c04d119bddd51a62447aa6f8d06378f6e3
sasi_p <sweetpea_th@hotmail.com>
2020-10-10 22:43:19 -0400

    Added DOM for unknown author and the font size for long quotes.

--- script.js
+++ script.js
@@ -1,25 +1,56 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+
+
+
+
 let apiQuotes = [];
 
 // Show New Quote
 function newQuote() {
     // Pick a random quote from apiQuotes array
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-    console.log(quote); 
+   //Check if Author field is blank and replace it with 'Unknown'
+   if (!quote.author) {
+       authorText.textContent = 'Unknown';
+   } else {
+       authorText.textContent = quote.author;
+   }
+   // Check Quote length to determine styling
+   if (quote.text.length > 50) {
+       quoteText.classList.add('long-quote');     
+   } else {
+       quoteText.classList.remove('long-quote');
+   }
+    quoteText.textContent = quote.text;
 }
 
 //Get Quote From API
-async function getQuotes() { 
+ async function getQuotes() { 
     const apiUrl = 'https://type.fit/api/quotes';
     try {
         const response = await fetch(apiUrl);
         apiQuotes = await response.json();
         newQuote();
     } catch (error) {
-      //Catch Error Here
-   
+       //Catch Error Here
     }
-}
+ }
 
 
 //On Load
 getQuotes();
+
+
+// To get quotes from local file
+//function newQuote() {
+    // Pick a random quote from localQuotes array
+    //const quote = localQuotes[Math.floor(Math.random() * localQuotes.length)];
+    //console.log(quote);
+//}
+
+// On Load
+//newQuote();
